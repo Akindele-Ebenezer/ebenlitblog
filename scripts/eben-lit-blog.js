b717529d705b9c7400b4615544e0c6275120404f
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-11-16 05:45:56 -0800

    Update eben-lit-blog.js

--- scripts/eben-lit-blog.js
+++ scripts/eben-lit-blog.js
@@ -9,3 +9,19 @@ const toggle = function() {
 navImg.addEventListener("click", toggle);
 
  //console.log(navImg);
+const dropDown = document.querySelector(".drop-down");
+const dropDown2 = document.querySelector(".drop-down-2");
+
+const contactDropArrow = document.querySelector("#header-2-wrapper li:nth-child(2)");
+const contactDropArrow2 = document.querySelector("#header-2-wrapper li:nth-child(4)");
+
+contactDropArrow.addEventListener("click", () => {
+	dropDown.classList.toggle("block");
+});
+ 
+contactDropArrow2.addEventListener("click", () => {
+	dropDown2.classList.toggle("block");
+});
+
+
+ 
