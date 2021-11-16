51f7ad827265d4e83f9553a50db70b1ffb670b2c
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-11-16 05:42:23 -0800

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&family=Zen+Kaku+Gothic+New:wght@500&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&family=Zen+Kaku+Gothic+New:wght@500&display=swap" rel="stylesheet">
 <header class="lg-screen-header">
 			<div id="header-1-wrapper">
 				<a href="index.php">
@@ -21,10 +21,28 @@
 			<div id="header-2-wrapper">
 					<ul>
 						<li><a href="index.php">HOME</a></li>
-						<li><a href="contact.php" <img src="images/drop-arrow.PNG" class="drop-arrow">CONTACT</a></li>
+						<li>CONTACT <img src="images/drop-arrow.png" class="drop-arrow"></li>
 						<li><a href="about.php">ABOUT</a></li>
-						<li><a href="blog.php" <img src="images/drop-arrow.PNG" class="drop-arrow">BLOG</a></li>
+						<li>BLOG <img src="images/drop-arrow.png" class="drop-arrow"></li>
 						<li><a href="articles.php">ARTICLES</a></li>
 					</ul> 			
+			</div>
+			<div class="drop-down drop-downs">
+				<ul>
+					<li><a href="https://api.whatsapp.com/send?phone=2349052757805" target="_blank">WHATSAPP</a></li>
+					<li><a href="mailto:tobi.akindele@gmail.com" target="_blank">GMAIL</a></li>
+					<li><a href="https://www.facebook.com/akindele.ebenezer/" target="_blank">FACEBOOK</a></li>
+					<li><a href="https://www.linkedin.com/in/akindele-ebenezer-566a16b8/">LINKEDIN</a></li>
+					<li><a href="https://www.instagram.com/ebenezer__/?hl=en" target="_blank">INSTAGRAM</a></li>
+				<ul>
+			</div>
+			<div class="drop-down-2 drop-downs">  
+				<ul>
+					<h1>Categories</h1>
+					<li><a href="poems.php">Poems</a></li>
+					<li>Articles</li>
+					<li><a href="interesting-stories.php">Interesting Stories</a></li>
+					<li><a href="technology.php">Technology</a></li>			
+				</ul>
 			</div>
 </header>
