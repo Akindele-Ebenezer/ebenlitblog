5d79a42b855f9912cd71e1a27685dbabe34f9231
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-11-16 05:42:02 -0800

    Update header-sm-screen.php

--- header-sm-screen.php
+++ header-sm-screen.php
@@ -18,6 +18,24 @@
 				<li><a href="about.php">ABOUT</a></li>
 				<li><a href="blog.php">BLOG</a></li>
 				<li><a href="articles.php">ARTICLES</a></li>
-			</ul>				
+			</ul>
+	<div class="sm-drop-down sm-drop-downs">
+		<ul>
+			<li><a href="https://api.whatsapp.com/send?phone=2349052757805" target="_blank">WHATSAPP</a></li>
+			<li><a href="mailto:tobi.akindele@gmail.com" target="_blank">GMAIL</a></li>
+			<li><a href="https://www.facebook.com/akindele.ebenezer/" target="_blank">FACEBOOK</a></li>
+			<li><a href="https://www.linkedin.com/in/akindele-ebenezer-566a16b8/">LINKEDIN</a></li>
+			<li><a href="https://www.instagram.com/ebenezer__/?hl=en" target="_blank">INSTAGRAM</a></li>
+		<ul>
+	</div>
+	<div class="sm-drop-down-2 sm-drop-downs">  
+		<ul>
+			<h1>Categories</h1>
+			<li><a href="poems.php">Poems</a></li>
+			<li>Articles</li>
+			<li><a href="interesting-stories.php">Interesting Stories</a></li>
+			<li><a href="technology.php">Technology</a></li> 
+		</ul>
+	</div>				
 	</div>
 </header>
