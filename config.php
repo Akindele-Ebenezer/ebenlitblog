35d4c96651277dcd0f884b5149d1ddd0b192defe
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-11-18 06:14:55 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
 
-$conn = mysqli_connect("us-cdbr-east-04.cleardb.com", "b3c2c4ce4a043d", "059428fa", "ebenlitblog");
+$conn = mysqli_connect("us-cdbr-east-04.cleardb.com", "b3c2c4ce4a043d", "059428fa", "heroku_01c2c2037f88863");
