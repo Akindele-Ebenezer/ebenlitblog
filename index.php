30c29a1d101e98018ddde7c520bb89596f7b1b69
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-11-19 19:37:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
 			</section>
 			<section id="main-section-2">
 				<div class="ads">
-					<a href="https://kol.jumia.com/api/click/banner/fb5361d9-79e5-4021-9c50-1c9832a40e8a/08103aea-a1a0-487f-bd06-57b83cb7c09d"><img src="https://kol.jumia.com/banners/hlDB0JiuTf8V9CYmzkYuV78k6EDQQDhC9G7OKoWY.jpg" alt="Jumia Black Friday Top Categories"/></a>
+					<a target="_blank" href="https://kol.jumia.com/api/click/banner/fb5361d9-79e5-4021-9c50-1c9832a40e8a/08103aea-a1a0-487f-bd06-57b83cb7c09d"><img src="https://kol.jumia.com/banners/hlDB0JiuTf8V9CYmzkYuV78k6EDQQDhC9G7OKoWY.jpg" alt="Jumia Black Friday Top Categories"/></a>
 				</div>
 					<!-- Some slide images here -->
 				<h1>Latest Blog Posts </h1>
