93e21977bd7bd64ed30452527c6a3d9d3b527d8d
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-11-22 05:31:38 -0800

    Update orji.php

--- orji.php
+++ orji.php
@@ -6,7 +6,7 @@ if(isset($_POST["submit"])) {
 	$comment = $_POST["comment"];
 	$date = $_POST["date"];
 
-	$sql_1 = "INSERT INTO orji_comments (first_name, last_name, comment, date) VALUES ('$first_name', '$last_name', '$comment', '$date');";
+	$sql_1 = "INSERT INTO orji_comments (first_name, last_name, comment, date) VALUES ('$first_name', '$last_name', '$comment', now());";
 	$query = mysqli_query($conn, $sql_1); 
 }
 
