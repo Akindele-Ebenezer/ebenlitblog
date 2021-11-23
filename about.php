279c2634257b2233eee01437eb9f068e8ed76784
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-11-23 19:27:16 +0100

    Update about.php

--- about.php
+++ about.php
@@ -12,7 +12,7 @@
 $title = "ABOUT ELB";
 $date = date("l jS \of F Y"); 
 $author = "Akindele Ebenezer";
-$content = "Welcome to <strong>Eben Lit Blog</strong>. This is a community where writers who are skilled in writing articles, interesting stories, poems, blogs etc. post their content to be viewed here on this website. <br><br>
+$content = "Welcome to <strong>WRITERS Crib</strong>. This is a community where writers who are skilled in writing articles, interesting stories, poems, blogs etc. post their content to be viewed here on this website. <br><br>
 If you're experienced in writing articles or blogs, you can <a href='contact.php' id='about-link'>send a direct message here</a>, with your content. Your blog will be uploaded instantly to this site. <br><br>
 - For more information, <a href='tel:09052757805' id='about-link'>call us</a> or <a href='https://api.whatsapp.com/send?phone=2349052757805' id='about-link'>send a whatsapp message</a>.";
 
