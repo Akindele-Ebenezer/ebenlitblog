84d8b347682d0b104be51b33dd8919aebc0b5ae7
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-11-25 02:37:03 -0800

    Update subscribed.php

--- subscribed.php
+++ subscribed.php
@@ -4,18 +4,18 @@
 	}
 </style>
 <?php
-include "config.php";
-if(isset($_POST["submit"])) {  
-	$email = $_POST["email"];
+	include "config.php";
+	if(isset($_POST["submit"])) {  
+			$email = $_POST["email"];
 
-	$sql_inbox = "INSERT INTO admin_inbox_subscribers (email, date) VALUES ('$email', now());";
-	$query_inbox = mysqli_query($conn, $sql_inbox);
-}
+			$sql_inbox = "INSERT INTO admin_inbox_subscribers (email, date) VALUES ('$email', now());";
+			$query_inbox = mysqli_query($conn, $sql_inbox);
 
 
-$author = "Ebenezer";
-$title = "Subscribed...";
-$content = "We will be sending interesting contents to $email. <br>EXPLORE...";
+		$author = "Ebenezer";
+		$title = "Subscribed...";
+		$content = "We will be sending interesting contents to $email. <br>EXPLORE...";
 
-include "blog-temp.php";
+		include "blog-temp.php";
+	}
 ?>
