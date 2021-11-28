ec348b1f4b327dc60d8ff69ef012b5a68bab5eb4
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-11-28 16:52:47 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 <head>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4278961694460844"
+     crossorigin="anonymous"></script>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-213761854-1"></script>
 <script>
