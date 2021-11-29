0ec25ecb450783f06bc13a42bc9fca6f9f840704
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-11-29 12:14:20 +0100

    Update header-sm-screen.php

--- header-sm-screen.php
+++ header-sm-screen.php
@@ -16,7 +16,7 @@
 				<li><a href="index.php">HOME</a></li>
 				<li><a href="contact.php">CONTACT</a></li>
 				<li><a href="about.php">ABOUT</a></li>
-				<li><a href="blog.php">BLOG</a></li>
+				<li><a href="blog.php">UNIQUE POSTS</a></li>
 				<li><a href="articles.php">ARTICLES</a></li>
 			</ul>
 	<div class="sm-drop-down sm-drop-downs">
